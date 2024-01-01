33299929505850eb7fe732058fc8aeb02f9a4508
mins00oo <alstn7223@gmail.com>
2024-01-02 03:36:50 +0900

    JPA 학습중
    mapped by를 명시해줘야 하는 이유를 학습했다. 또한, Json직렬화 문제 때문에 JsonManagedReference 어노테이션을 사용해주고 team과 user간 fetch join을 사용해서 효율적 쿼리를 학습해봄

--- src/main/java/com/example/jpa_test/User/User.java
+++ src/main/java/com/example/jpa_test/User/User.java
@@ -1,17 +1,23 @@
 package com.example.jpa_test.User;
 
 import com.example.jpa_test.team.Team;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String firstName;
     private String lastName;
 
-    @ManyToOne(fetch = FetchType.EAGER)		// 지연 로딩
+    @ManyToOne(fetch = FetchType.LAZY)        // 지연 로딩
     @JoinColumn(name = "team_id", nullable = false)
+    @JsonBackReference
     private Team team;
 }
--- src/main/java/com/example/jpa_test/team/Team.java
+++ src/main/java/com/example/jpa_test/team/Team.java
@@ -1,18 +1,25 @@
 package com.example.jpa_test.team;
 
 import com.example.jpa_test.User.User;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Team {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
 
-    @OneToMany(fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "team", fetch = FetchType.EAGER)
+    @JsonManagedReference
     private List<User> users = new ArrayList<>();
 }
